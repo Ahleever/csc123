676dff1abcaf37a7622100db45c5a13ce36f87a5
Ahleever <monterooliver97@gmail.com>
2023-09-02 15:59:24 -0700

    Added "Hey"

--- csc123/src/csc123/HelloWorld.java
+++ csc123/src/csc123/HelloWorld.java
@@ -3,8 +3,8 @@ package csc123;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		System.out.println("Hey");
 	}
 
 }
